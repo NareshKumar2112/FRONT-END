b866b4a785b718ea6a5a7e380e82b7c91472cd88
NareshKumar2112 <125951445+NareshKumar2112@users.noreply.github.com>
2023-03-08 12:32:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Cricket from './Cricket';
+import Get from './Get';
+import Delete from './Delete'
+function App() {
+  return (
+    <div>
+      <Cricket/>
+      <Get/>
+      <Delete/>
+    </div>
+  );
+}
+
+export default App;
